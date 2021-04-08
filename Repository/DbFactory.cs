749a1e9ddcdde61e85048258ec1ce5f2c7e00921
JeffWu0624 <jeffwu0624@hotmail.com>
2021-04-09 00:09:04 +0800

    加入專案檔案。

---
+++ Repository/DbFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data.Common;
+
+namespace Repository
+{
+    public class DbFactory
+    {
+        public static DbConnection CreateConnection(string connectionName)
+        {
+            var settings = ConfigurationManager.ConnectionStrings[connectionName];
+
+            var factory = DbProviderFactories.GetFactory(settings.ProviderName);
+
+            var connection = factory.CreateConnection();
+
+            connection.ConnectionString = settings.ConnectionString;
+
+            return connection;
+        }
+    }
+}
---
+++ Repository/UserRepo.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Dapper;
+
+namespace Repository
+{
+    public class UserRepo 
+    {
+        public UserInfo GetUserBy(UserQueryCondition condition)
+        {
+            UserInfo result = null;
+
+            var builder = new SqlBuilder();
+
+            var template = builder.AddTemplate("select id, name from user /**where**/ ");
+
+            if (condition.id.HasValue)
+            {
+                builder.Where("id = @Id", new {Id = condition.id});
+            }
+
+            if (!string.IsNullOrWhiteSpace(condition.Name))
+            {
+                builder.Where("Name = @Name", new {Name = condition.Name.Trim()});
+            }
+
+            using (var cn = DbFactory.CreateConnection(""))
+            {
+                result = cn.Query<UserInfo>(template.RawSql, template.Parameters).FirstOrDefault();
+
+                cn.Close();
+            }
+
+            return result;
+        }
+    }
+
+    public class UserQueryCondition
+    {
+        public int? id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class UserInfo
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
